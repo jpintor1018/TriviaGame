1811dc8e8e7e10cf0ab2e3cdb3e0fae09106faf5
John Michael Pintor <jpintor1018@yahoo.com>
2019-07-23 22:38:06 -0400

    Questions/Answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
     var startTime=6;
     var correctCount=0;
     var wrongCount=0;
+    var unanswered=0;
+    // var overall = triviaQuest.length;
     var questArray;
     
     
@@ -20,20 +22,48 @@ $(document).ready(function(){
 
        //ARRAY OF QUESTIONS/CHOICES/ANSWERS
         let triviaQuest = [
-           { Question: "Where Can You Watch 'Stranger Things'?", 
-            Choice: ["Amazon Prime", "Netflix", "Hulu", "YouTube",],
-            answer: "1"
-           },
-                { Question: "Who does Millie Bobby Brown play as?", 
-                    Choice: ["Joyce", "Nancy", "Eleven", "Max",],
-                    answer: "2"
-                }, 
-                    { Question: "What game does Mike, Dustin, Lucas, and Will play?", 
-                        Choice: ["Atari", "Dungeon&Dragons", "Donkey Kong", "Pac-Man",],
-                        answer: "1"
-        },
+            {Question: "What year does season one take place?", 
+             Choice: ["1987", "1990", "1983", "1985"],
+             answer: "2"
+            },
+            {Question: "Who was the character from 'Ghostbusters' that Mike and Lucas both ended up dressing as?", 
+             Choice: ["Winston", "Egon", "Venkman", "Stance"],
+             answer: "2"
+            }, 
+            {Question: "What is the name of the game that Mike, Dustin, Lucas, and Will play?", 
+             Choice: ["Atari", "Dungeon&Dragons", "Dragon's Lair", "Magic: The Gathering"],
+             answer: "1"
+            },
+            {Question: "What is the name of the unusual disorder Dustin has?", 
+             Choice: ["Epidermolysis", "Cleidocranial Dysplasia", "Hydrocephalus", "Lamellar Ichthyosis"],
+             answer: "1"
+            },
+            {Question: "What do Mike, Lucas, Dustin call the terrifying, faceless creature?", 
+             Choice: ["Wookie", "Godzilla", "The Thing", "Demogorgon"],
+             answer: "3"
+            },
+            {Question: "How does Joyce communicate with her son Will when he disappeared?", 
+             Choice: ["Painting on the walls", "Through a mirror", "Christmas Lights", "Walkie Talkie"],
+             answer: "2"
+            },
+            {Question: "The parallel dimension inhabited by the Demogorgon is referred to as ____?", 
+             Choice: ["The Butterfly Effect", "The Dark World", "The Upside Down", "The Death Star"],
+             answer: "2"
+            },
+            {Question: "What does Jonathan give Will as he recovers in the hospital?", 
+             Choice: ["Nintendo", "A Movie", "A Poster", "A Mixtape"],
+             answer: "3"
+            },
+            {Question: "What is the name of the ice cream shop Steve and Robin work in the mall?", 
+             Choice: ["Chips Ahoy", "Ahoy Ahoy", "Scoops Ahoy", "Ice Cream CONEection"],
+             answer: "2"
+            },
+            {Question: "What is the name of the song that Suzie asks Dusitn to sing?", 
+             Choice: ["Total Eclipse of the Heart", "Every Breath You Take", "Girls Just Want To Have Fun", "The Neverending Story"],
+             answer: "3"
+            },
         ]
-        console.log(triviaQuest[2])
+   
 
 //HAVE 'VAR INDEX' AS A RANDOM NUMBER
 //SO THE QUESTIONS WILL BE RANDOMIZED WHEN DISPLAYED ON THE SCREEN
@@ -41,11 +71,13 @@ function Questions(){
     var index = Math.floor(Math.random()*triviaQuest.length)
     console.log("index " + index)
 
+
 //'VAR questARRY' WILL TAKE IN THE OBJECTS IN THE ARRAY
     questArray = triviaQuest[index];
     console.log(questArray)
     
 // DISPLAY THE RANDOMIZED QUESTION ON SCREEN
+    
     $("#questions").html("<h2>" + questArray.Question + "</h2>")
 // FOR LOOP TO SHOW THE DIFFERENT OPTIONS USER CAN PICK FROM
     for(let i=0; i<questArray.Choice.length; i++)
@@ -61,6 +93,7 @@ function Questions(){
     }
 
 }
+
 
 //ONCLICK FUNCTION WHERE USER CAN CLICK ON THE ANSWER
 $(document).on("click", '.userChoice', function(){
@@ -80,15 +113,15 @@ $(document).on("click", '.userChoice', function(){
         correctCount++;
         $("#choices").html("<p>Correct!</p>")
         console.log(guess)
+        guess="";
+        Questions();
     }
     else 
     {
         wrongCount++;
         $("#choices").html("Wrong")
+        Questions();
     }
-
-    
-
 });
 Questions();
 
